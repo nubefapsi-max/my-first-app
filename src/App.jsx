3298ca70cb129379b11367038df01862125c05e1
nubefapsi-max <nubefapsi@gmail.com>
2025-11-08 15:25:29 -0600

    Agregue mensjae dentro de etiqueta P

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React  My First App</h1>
+      <p>Bienvenidos a my App</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
